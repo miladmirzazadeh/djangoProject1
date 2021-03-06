a0bb254919855f7760955aa37c25a80733291c58
Milad <milad.mirzazadeh@yahoo.com>
2021-03-06 18:20:28 +0330

    for-first

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import HttpResponse
+from django.template import loader
+def response(request):
+    template = loader.get_template('blog/login2.html')
+    content = template.render(context={}, request=request)
+    return(HttpResponse(content))
