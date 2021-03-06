c484669dfb938befaeea49c6812b1f366c6adfdb
Milad <milad.mirzazadeh@yahoo.com>
2021-03-06 18:33:55 +0330

    debug=False

--- djangoProject_first/settings.py
+++ djangoProject_first/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#jr6iierqg@3jltu@2bm+$84hlxted$aq**4cz@(ozatc+y!58'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
